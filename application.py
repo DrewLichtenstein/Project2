bd35b12a8ea7986c00720690d7450e243858856a
Drew Lichtenstein <drew_lichtenstein@harvard.edu>
2018-07-20 11:25:39 +0000

    looking good!

--- application.py
+++ application.py
@@ -13,9 +13,9 @@ channel_list = ['general']
 # message class
 
 class Message:
-    def __init__(self, channel, time_stamp, username, comment):
+    def __init__(self, channel, timestamp, username, comment):
         self.channel = channel
-        self.time_stamp = time_stamp
+        self.timestamp = timestamp
         self.username = username
         self.comment = comment
 
@@ -36,11 +36,13 @@ def making_all_channels_onload():
     emit("list_all_the_channels", channel_list, broadcast=True)
 
 @socketio.on("message_sent")
-def handle_message(message, chatroom_name, current_username, timestamp):
-    readable_time = datetime.datetime.fromtimestamp(timestamp)
-    readable_time_string = readable_time.strftime('%Y-%m-%d %H:%M:%S')
-    message_information = Message(channel=chatroom_name, time_stamp=readable_time_string, username=current_username, comment=message)
-    message_list.append(message_information)
+def handle_message(message, chatroom_name, current_username, timestamp_string):
+    message_information = Message(channel=chatroom_name, timestamp=timestamp_string, username=current_username, comment=message)
+    if len(message_list) < 100:
+        message_list.append(message_information)
+    else:
+        message_list.pop()
+        message_list.append(message_information)
     emit("show_message", message, broadcast=True)
 
 @socketio.on("get_message_history")
@@ -48,9 +50,11 @@ def handing_off_messages (chatroom_name):
     individual_channel_messages = []
     for x in message_list:
         if x.channel == chatroom_name:
-            individual_channel_messages.append(x.time_stamp + " " + x.username + ": " +x.comment)
-            print(x.comment)
-    else:
-        print("no")
-
+            individual_channel_messages.append(x.timestamp + " " + "["+ x.username +"]" + ": " +x.comment)
     emit('show_all_messages', individual_channel_messages)
+
+@socketio.on("delete_message_history")
+def find_message_and_delete (message, chatroom_name, current_username, timestamp_string):
+    for x in message_list:
+        if x.channel == chatroom_name and x.timestamp == timestamp_string and x.comment == message:
+            message_list.remove(x)
--- static/index.js
+++ static/index.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     else {
         var chatroom_name = localStorage.getItem("last_seen_chatroom");
-        alert("Seeing message history from " + chatroom_name + ". Click a channel to enter.")
+        alert("Seeing message history from channel " + chatroom_name + ". Click a channel to enter.")
         socket.emit("get_message_history", chatroom_name);
     }
 
@@ -59,25 +59,32 @@ socket.on("list_all_the_channels", put_all_channels_in_html);
 var render_chat = function () {
     document.querySelector("#message_container_div").style.visibility = "visible";
     document.querySelector("#message_list").innerHTML = "";
-
-    // fix the timestamp stuff
-    var date = new Date();
-    console.log(date);
-    var timestamp = date.getTime();
+    var timestamp = new Date();
+    var timestamp_string = String(timestamp);
     var current_username = localStorage.getItem("username");
     var chatroom_name = this.id;
-    alert("Welcome to " + chatroom_name)
+    document.getElementById('channel_header').innerHTML = chatroom_name;
     localStorage.setItem("last_seen_chatroom", chatroom_name);
     socket.emit("get_message_history", chatroom_name)
 
     document.querySelector("#message_sender").onsubmit = () => {
         var message = document.getElementById("message").value;
-        socket.emit("message_sent", message, chatroom_name, current_username, timestamp);
+        socket.emit("message_sent", message, chatroom_name, current_username, timestamp_string);
         return false;};
 
     function display_message (message) {
         const li = document.createElement('li');
-        li.innerHTML = timestamp + " "+ current_username + " "+ message;
+        var button = document.createElement("BUTTON");
+        var delete_chat = function () {
+            socket.emit("delete_message_history", message, chatroom_name, current_username, timestamp_string)
+            this.parentNode.parentNode.removeChild(this.parentNode)
+        };
+        button.onclick = delete_chat;
+        var button_text = document.createTextNode("Delete Message");
+        button.appendChild(button_text);
+        li.innerHTML = timestamp_string + " "+ "[" + current_username + "]" + ":"+ message;
+        li.appendChild(button);
+        console.log(localStorage.getItem("username"));
         document.querySelector('#message_list').append(li);};
 
     socket.on("show_message", display_message);
